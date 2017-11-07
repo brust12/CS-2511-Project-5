734ca6c3a276935537e25c6e45ad01ac84721638
Riley B <33437467+brust12@users.noreply.github.com>
2017-11-06 21:12:10 -0600

    IsonPath working

---
+++ src/framework/solution/StateSpaceSolver.java
@@ -0,0 +1,84 @@
+package framework.solution;
+
+import framework.graph.Vertex;
+import framework.problem.Problem;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * This class represents a state space solver by extending the abstract
+ * Solver class.
+ * @author Your name and section here
+ */
+public class StateSpaceSolver extends Solver {
+    
+    /**
+     * Creates a BFS or DFS state space solver.
+     * This constructor should set the queue to a double-ended queue (DEQUE)
+     * and set the statistics header.
+     * @param problem the problem being solved
+     * @param bfs a boolean flag indicating whether BFS is to be performed
+     */
+    public StateSpaceSolver(Problem problem, boolean bfs) {
+        super(problem);
+        /* you must provide */
+    }
+    
+    /**
+     * Adds a vertex to the DEQUE.
+     * If BFS is being performed, the vertex should be added to the
+     * end of the DEQUE, so the DEQUE acts like an ordinary queue.
+     * If DFS is being performed, the vertex should be added to the
+     * front of the DEQUE, so the DEQUE acts like a stack.
+     * @param v 
+     */
+    @Override
+    public void add(Vertex v) {
+	/* you must provide */
+    }
+    
+    /**
+     * This method implements the expand operation required by the 
+     * state space search algorithm:
+
+       Expand(u)
+          children = {}
+          for each name ∈ moveNames do
+             child = mover.doMove(name, u)
+             if child ≠ null and not OccursOnPath(child, u)
+                then d[child] = d[u] + 1
+                     pred[child] = u
+                     add child to children
+          return children
+
+     * @param u the vertex being expanded
+     * @return 
+     */
+    @Override
+    public List<Vertex> expand(Vertex u) {
+        List<Vertex> list;
+        list = new ArrayList();
+    return list;
+    }
+
+    /**
+     * Checks whether a given vertex appears on the predecessor path
+     * of a given ancestor.
+     * @param v the vertex to check
+     * @param ancestor the ancestor vertex of v
+     * @return true if v occurs on the predecessor path of ancestor,
+     * false otherwise
+     */
+    public static boolean occursOnPath(Vertex v, Vertex ancestor) {
+        while(ancestor != null){
+            if(ancestor.getData().equals( v.getData())){
+            return true;
+            }
+            ancestor = ancestor.getPredecessor();
+        }
+        return false;
+    }
+    
+    /* private instance fields here */
+}
